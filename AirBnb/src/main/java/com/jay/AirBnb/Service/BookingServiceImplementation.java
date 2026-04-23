f3ebb0f2d519008b610510f259fb2f984ba9ffa0
jaygajora <gajorajay@gmail.com>
2026-04-23 01:43:22 -0700

    Extended Booking Logic: addGuests(), getBookingStatusById(), hasBookingExpired()

--- AirBnb/src/main/java/com/jay/AirBnb/Service/BookingServiceImplementation.java
+++ AirBnb/src/main/java/com/jay/AirBnb/Service/BookingServiceImplementation.java
@@ -2,17 +2,17 @@ package com.jay.AirBnb.Service;
 
 import com.jay.AirBnb.Dto.BookingDTO;
 import com.jay.AirBnb.Dto.BookingRequest;
+import com.jay.AirBnb.Dto.GuestDTO;
 import com.jay.AirBnb.Dto.InventoryDTO;
 import com.jay.AirBnb.Entity.*;
 import com.jay.AirBnb.Enums.BookingStatus;
+import com.jay.AirBnb.Enums.Gender;
 import com.jay.AirBnb.Exceptions.ResourceNotFoundException;
 import com.jay.AirBnb.Exceptions.UnauthorisedException;
-import com.jay.AirBnb.Repository.BookingRepository;
-import com.jay.AirBnb.Repository.HotelRepository;
-import com.jay.AirBnb.Repository.InventoryRepository;
-import com.jay.AirBnb.Repository.RoomRepository;
+import com.jay.AirBnb.Repository.*;
 import com.jay.AirBnb.Service.Interface.BookingService;
 import com.jay.AirBnb.Strategy.PricingService;
+import org.apache.coyote.BadRequestException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,9 +21,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 @Service
 public class BookingServiceImplementation implements BookingService {
@@ -42,6 +44,9 @@ public class BookingServiceImplementation implements BookingService {
 
     @Autowired
     private PricingService pricingService;
+
+    @Autowired
+    private GuestRepository guestRepository;
 
     @Autowired
     ModelMapper modelMapper;
@@ -109,6 +114,59 @@ public class BookingServiceImplementation implements BookingService {
     }
 
     // add Guests
+    @Override
+    @Transactional
+    public BookingDTO addGuests(Long bookingId, List<GuestDTO> guests){
+        if(bookingId == null){
+            throw new IllegalArgumentException("Booking Id CANNOT be EMPTY | NULL");
+        }
+
+        BookingEntity booking = bookingRepository.findById(bookingId).orElseThrow(() -> new ResourceNotFoundException("No booking found with booking id: " + bookingId));
+
+        UserEntity user = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if(!user.equals(booking.getUser())){
+            throw new UnauthorisedException("You did not initialise this booking and hence you CANNOT ADD GUESTS to this booking");
+        }
+
+        if(hasBookingExpired(booking)){
+           throw new IllegalStateException("Your booking window has expired. TRY AGAIN!");
+        }
+
+        if(booking.getStatus() != BookingStatus.RESERVED){
+            throw new IllegalStateException("Your booking is not under RESERVED state, you cannot add guests now! (Your booking has been confirmed or Cancelled)");
+        }
+
+        for(GuestDTO guestDTO : guests){
+            String name = guestDTO.getName();
+            int age = guestDTO.getAge();
+            Gender gender = guestDTO.getGender();
+
+            // if guestId == null, BUT what if the guest has been added just while making the booking!
+
+            if(name == null || name.trim().isEmpty()){
+                throw new IllegalArgumentException("Please enter the name of the guest!");
+            }
+
+            if(age < 0 || age > 120){
+                throw new IllegalArgumentException("Age must be between 0 to 120");
+            }
+
+            if(gender == null){
+                throw new IllegalArgumentException("Please select " + name + "'s gender!");
+            }
+
+            GuestEntity guest = modelMapper.map(guestDTO, GuestEntity.class);
+            booking.setUser(user);     // isn't this reduntant as we are already setting the user while initializing the booking?
+            guestRepository.save(guest);
+            booking.getGuests().add(guest);
+        }
+
+        booking.setStatus(BookingStatus.GUESTS_ADDED);
+        bookingRepository.save(booking);
+
+        return modelMapper.map(booking, BookingDTO.class);
+    }
 
     // initiate Payments
 
@@ -117,14 +175,39 @@ public class BookingServiceImplementation implements BookingService {
     // cancel booking
 
     // get booking status
+    public BookingStatus getBookingStatusById(Long bookingId){
+        if(bookingId == null){
+            throw new IllegalArgumentException("Booking id CANNOT be EMPTY | NULL");
+        }
 
-    // get All bookigns by HotelId
+        BookingEntity booking = bookingRepository.findById(bookingId).orElseThrow(() -> new ResourceNotFoundException("No booking found with booking id: " + bookingId));
+        UserEntity user = (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if(!user.equals(booking.getUser())){
+            throw new UnauthorisedException("This booking does not belong to you and hence you CANNOT access its BOOKING STATUS!");
+        }
+
+        BookingStatus status = booking.getStatus();
+
+        if(status == BookingStatus.RESERVED || status == BookingStatus.GUESTS_ADDED){
+            if(hasBookingExpired(booking)){
+                throw new IllegalStateException("Your booking window has expired! Please TRY AGAIN!");
+            }
+        }
+
+        return status;
+    }
+
+    // get All bookings by HotelId
 
     // get Hotel Report
 
     // get my booking (user's booking/ bookingsByUser)
 
     //hasBookingExpired
+    public boolean hasBookingExpired(BookingEntity booking){
+        return booking.getCreateAt().plusMinutes(10).isBefore(LocalDateTime.now());
+    }
 
 
 
--- AirBnb/src/main/java/com/jay/AirBnb/Service/Interface/BookingService.java
+++ AirBnb/src/main/java/com/jay/AirBnb/Service/Interface/BookingService.java
@@ -2,7 +2,13 @@ package com.jay.AirBnb.Service.Interface;
 
 import com.jay.AirBnb.Dto.BookingDTO;
 import com.jay.AirBnb.Dto.BookingRequest;
+import com.jay.AirBnb.Dto.GuestDTO;
+import com.jay.AirBnb.Enums.BookingStatus;
+
+import java.util.List;
 
 public interface BookingService {
     BookingDTO initializeBooking(BookingRequest bookingRequest);
+    BookingDTO addGuests(Long bookingId, List<GuestDTO> guests);
+    BookingStatus getBookingStatusById(Long bookingId);
 }
